d6e9d2b89545cee33fa08434e807251f4156af1c
Rina226 <1977notw.q@gmail.com>
2021-03-16 09:36:56 +0900

    0315_Slideshow_Fade

---
+++ script/script.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    setInterval(fnSlide, 3000);
+    function fnSlide() {
+
+        /*작업 순서
+        1. 모든 a요소를 fadeout한다
+        2. 2번째 a요소를 fadein 한다.
+        fadein 완료 후 콜백함수를 사용하여 첫째 a요소를 마지막으로 이동한다*/
+
+        $("#slideArea a").fadeOut(500);
+        $("#slideArea a:nth-child(2)").fadeIn(
+            1000,
+            function() {
+                $("a:first-child").insertAfter("a:last-child");
+            }
+        );
+    }
+
+});
